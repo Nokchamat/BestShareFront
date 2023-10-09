cd1b7b51ee93efd4bd2276f02172b369c7f7913c
NokchaMat <rkdtjd976@gmail.com>
2023-10-09 18:51:10 +0900

    ### SignIn - 로그인 요청 후 에러 발생 시 모달창 뜨고 닫기 구현

--- src/views/Sign.vue
+++ src/views/Sign.vue
@@ -14,24 +14,42 @@
     </ul>
 
     <!-- Form -->
-    <form  action="" method="post">
+    <div class="form">
       <!-- email input -->
-      <div class="first-input input__block first-input__block">
-        <input type="email" placeholder="Email" class="input" id="email"   />
+      <div v-bind:class="selectBlock ? 'first-input__block' : 'signup-input__block'"
+           class="first-input input__block">
+        <input v-model="email" type="email" placeholder="이메일" class="input" id="email"/>
       </div>
       <!-- password input -->
       <div class="input__block">
-        <input type="password" placeholder="Password" class="input" id="password"    />
+        <input v-model="password" type="password" placeholder="비밀번호" class="input" id="password"/>
       </div>
       <!-- repeat password input -->
       <div class="input__block">
-        <input type="password" placeholder="Repeat password" class="input repeat__password" id="repeat__password"    />
+        <input v-model="repeatPassword" type="password" placeholder="비밀번호 확인"
+               v-bind:class="{'repeat__password': selectBlock}" class="input"
+               id="repeat__password"/>
+      </div>
+      <!-- name input -->
+      <div class="input__block">
+        <input v-model="name" type="text" placeholder="이름"
+               v-bind:class="{'repeat__password': selectBlock}" class="input" id="name"/>
+      </div>
+      <!-- nickname input -->
+      <div class="input__block">
+        <input v-model="nickname" type="text" placeholder="닉네임"
+               v-bind:class="{'repeat__password': selectBlock}" class="input" id="nickname"/>
+      </div>
+      <!-- phoneNumber input -->
+      <div class="input__block">
+        <input v-model="phoneNumber" type="number" placeholder="휴대전화 예시)01012341234"
+               v-bind:class="{'repeat__password': selectBlock}" class="input" id="phone__number"/>
       </div>
       <!-- sign in button -->
-      <button class="signin__btn">
-        Sign in
+      <button @click="selectBlock ? postSignIn() : postSignUp()" class="signin__btn">
+        {{ bottomButton }}
       </button>
-    </form>
+    </div>
     <!-- separator -->
     <div class="separator">
       <p>OR</p>
@@ -48,39 +66,17 @@
     </button>
   </div>
 
-  <footer>
-    <p>
-      <i class="fa fa-heart"></i>
-      <i class="fa fa-heart"></i>
-      <i class="fa fa-heart"></i>
-    </p>
-    <p>
-      <a href="https://www.facebook.com/chouaib45" >
-        <i class="fa fa-facebook"></i>
-      </a>
-      <a href="http://twitter.com/chouaibblgn45">
-        <i class="fa fa-twitter"></i>
-      </a>
-      <a href="http://instagram.com/chouaib_blgn">
-        <i class="fa fa-instagram"></i>
-      </a>
-      <a href="http://linkedin.com/in/chouaibblgn/">
-        <i class="fa fa-linkedin"></i>
-      </a>
-      <a href="https://www.behance.net/geek30">
-        <i class="fa fa-behance"></i>
-      </a>
-    </p>
-  </footer>
+  <Modal v-show="modalState" v-bind:modalData="modalData" v-model:modalState="modalState"/>
+
 </template>
 
-<style scoped src="@/assets/template/assets/sass/sign.scss" />
+<style scoped src="@/assets/template/assets/sass/sign.scss"/>
 
 <script>
 import axios from "axios";
 import Header from "@/components/layout/Header.vue";
 import pageShareBoard from "@/views/PageShareBoard.vue";
-
+import Modal from "@/views/Modal.vue";
 
 export default {
   computed: {
@@ -88,24 +84,69 @@ export default {
       return pageShareBoard
     }
   },
-  components: {Header},
+  components: {
+    Modal,
+    Header,
+  },
   data() {
     return {
-      title: "SIGN IN"
-
+      title: "SIGN IN",
+      bottomButton: "Sign in",
+      selectBlock: true,
+      email: "",
+      password: "",
+      repeatPassword: "",
+      name: "",
+      nickname: "",
+      phoneNumber: "",
+      modalState: false,
+      modalData: ""
     }
   },
   methods: {
     signIn() {
-
+      this.title = "SIGN IN"
+      this.bottomButton = "Sign in"
+      this.selectBlock = "first-input__block"
+      this.selectBlock = true
     },
     signUp() {
       this.title = "SIGN UP"
+      this.bottomButton = "Sign up"
+      this.selectBlock = false
+    },
+    postSignIn() {
+      axios.post("http://localhost:8080/v1/user/sign-in", {
+            email: this.email,
+            password: this.password
+          },
+          {
+            headers: {
+              'content-type': 'application/json',
+            }
+          })
+      .then(res => {
+        console.log(JSON.stringify(res.headers.get('Authorization')))
+      }).catch(err => {
+        console.log(err.response.data.message);
+        this.modalData = err.response.data.message
+        this.changeModalState()
+      });
+    },
+    postSignUp() {
+      axios
+      .post("http://localhost:8080/user/v1/sign-up")
+      .then((res) => {
+        console.log(res.data);
+        this.page = res.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    },
+    changeModalState() {
+      this.modalState = !this.modalState
     }
   },
-  mounted() {
-    this.getData();
-  },
 }
-
 </script>
